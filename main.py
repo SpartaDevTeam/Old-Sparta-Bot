f35166745023c3a2efdd41da7fbfcecc3f0ccabc
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-07-16 19:39:15 +0400

    OMG FINALLY THE WARN COMMAND WORKS

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ prefix = "s!"
 bot = commands.Bot(command_prefix=prefix,
                    description="Description yet to be added...")
 
+warn_count = {}
 
 @bot.event
 async def on_ready():
@@ -51,6 +52,39 @@ async def hello(ctx):
 async def ping(ctx):
     latency_ms = round(bot.latency * 1000)
     await ctx.send(f"Ping: {latency_ms}ms")
+
+
+@bot.command(name="addmodrole")
+async def addmodrole(ctx):
+    role = ctx.message.role_mentions[0]
+    if ctx.author.guild_permissions.administrator:
+        mod_roles = open("modlist.txt", "a")
+        mod_roles.writelines(str(role.id) + "\n")
+        mod_roles.close()
+        await ctx.send(f"Role {role} has been added to Moderator Roles list.")
+    else:
+        await ctx.send("You are not allowed to use this command!")
+
+
+@bot.command(name="warn")
+async def warn(ctx, user: discord.User, reason):
+    print("warning user")
+    mod_roles = open("modlist.txt", "r")
+    for role_id in mod_roles.readlines():
+        role_id = role_id.replace("\n", "")
+        if ctx.guild.get_role(int(role_id)) in ctx.author.roles:
+            if str(user) not in warn_count:
+                warn_count[str(user)] = 1
+            else:
+                warn_count[str(user)] += 1
+            embed = discord.Embed(title=f"{user.name} has been warned")
+            embed.add_field(name="Reason", value=reason)
+            embed.add_field(name="This user has been warned",
+                            value=f"{warn_count[str(user)]} time(s)")
+
+            await ctx.send(content=None, embed=embed)
+            break
+    mod_roles.close()
 
 
 bot.run(token)
