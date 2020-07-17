d2f09f72b5d0c436e6089c323c371f999dd43e1b
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-07-17 17:42:02 +0400

    Added some stuff

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ bot = commands.Bot(command_prefix=prefix,
 
 warn_count = {}
 
+
 @bot.event
 async def on_ready():
     server_count = len(bot.guilds)
-    activity = discord.Activity(type=discord.ActivityType.watching, name=f"{server_count} servers")
+    activity = discord.Activity(
+        type=discord.ActivityType.watching, name=f"{server_count} servers || {prefix}help")
     await bot.change_presence(activity=activity)
     print("Bot is ready...")
 
@@ -54,9 +56,12 @@ async def _help(ctx):
     embed = discord.Embed(title="Help")
     embed.add_field(name=f"{prefix}help", value="Displays command help")
     embed.add_field(name=f"{prefix}hello", value="Say hello to the bot")
-    embed.add_field(name=f"{prefix}ping", value="Get the bot's latency in milliseconds")
-    embed.add_field(name=f"{prefix}addmodrole", value="Give a role the permission to use Moderation commands")
-    embed.add_field(name=f"{prefix}warn", value="Warn a user for doing something")
+    embed.add_field(name=f"{prefix}ping",
+                    value="Get the bot's latency in milliseconds")
+    embed.add_field(name=f"{prefix}addmodrole",
+                    value="Give a role the permission to use Moderation commands")
+    embed.add_field(name=f"{prefix}warn",
+                    value="Warn a user for doing something")
 
     await ctx.author.send("Here is the command help:", embed=embed)
 
@@ -75,34 +80,40 @@ async def ping(ctx):
 @bot.command(name="addmodrole")
 async def addmodrole(ctx):
     role = ctx.message.role_mentions[0]
-    if ctx.author.guild_permissions.administrator:
-        mod_roles = open("modlist.txt", "a")
-        mod_roles.writelines(str(role.id) + "\n")
-        mod_roles.close()
-        await ctx.send(f"Role {role.mention} has been added to Moderator Roles list.")
+    if role != None:
+        if ctx.author.guild_permissions.administrator:
+            mod_roles = open("modlist.txt", "a")
+            mod_roles.writelines(str(role.id) + "\n")
+            mod_roles.close()
+            await ctx.send(f"Role {role.mention} has been added to Moderator Roles list.")
+        else:
+            await ctx.send("You are not allowed to use this command!")
     else:
-        await ctx.send("You are not allowed to use this command!")
+        await ctx.send("Please mention a role with the command.")
 
 
 @bot.command(name="warn")
 async def warn(ctx, user: discord.User, *, reason):
-    print(f"Warning user {user.name} for {reason}...")
-    mod_roles = open("modlist.txt", "r")
-    for role_id in mod_roles.readlines():
-        role_id = role_id.replace("\n", "")
-        if ctx.guild.get_role(int(role_id)) in ctx.author.roles:
-            if str(user) not in warn_count:
-                warn_count[str(user)] = 1
-            else:
-                warn_count[str(user)] += 1
-            embed = discord.Embed(title=f"{user.name} has been warned")
-            embed.add_field(name="Reason", value=reason)
-            embed.add_field(name="This user has been warned",
-                            value=f"{warn_count[str(user)]} time(s)")
+    try:
+        print(f"Warning user {user.name} for {reason}...")
+        mod_roles = open("modlist.txt", "r")
+        for role_id in mod_roles.readlines():
+            role_id = role_id.replace("\n", "")
+            if ctx.guild.get_role(int(role_id)) in ctx.author.roles:
+                if str(user) not in warn_count:
+                    warn_count[str(user)] = 1
+                else:
+                    warn_count[str(user)] += 1
+                embed = discord.Embed(title=f"{user.name} has been warned")
+                embed.add_field(name="Reason", value=reason)
+                embed.add_field(name="This user has been warned",
+                                value=f"{warn_count[str(user)]} time(s)")
 
-            await ctx.send(content=None, embed=embed)
-            break
-    mod_roles.close()
+                await ctx.send(content=None, embed=embed)
+                break
+        mod_roles.close()
+    except discord.ext.commands.errors.MissingRequiredArgument as error:
+        await ctx.send(error)
 
 
 bot.run(token)
