9fd904befd6c6794e5f4ec1fb310a69abe09f9dd
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-07-30 17:16:36 +0400

    removed temp msg logging

--- main.py
+++ main.py
@@ -263,7 +263,7 @@ async def whitelisturl(ctx, url: str):
 async def on_message(message):
     author = message.author
     channel = message.channel
-    print(str(author), ": ", message.content)
+    # print(str(author), ": ", message.content)
 
     await bot.process_commands(message)
 
